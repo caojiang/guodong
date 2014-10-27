0784d862c7d7622d2488f9856e47981928dcb0cd
lhtdeg <476183603@qq.com>
2014-10-27 18:17:13 +0800

    项目提交

---
+++ src/com/gdb/guodongbu/MainActivity.java
@@ -0,0 +1,63 @@
+package com.gdb.guodongbu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener{
+	TextView forgetpassword,register;
+	Button Login;
+	EditText password,account;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		forgetpassword=(TextView)findViewById(R.id.tv_main_fogetpassword);
+		register=(TextView)findViewById(R.id.tv_main_register);
+		Login=(Button)findViewById(R.id.btn_main_login);
+		password=(EditText)findViewById(R.id.et_main_password);
+		account=(EditText)findViewById(R.id.et_main_zhanghao);
+		Login.setOnClickListener(this);
+		forgetpassword.setOnClickListener(this);
+		register.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.tv_main_register:
+			Intent intent=new Intent(MainActivity.this,RegisterFrame.class);
+			startActivity(intent);break;
+		case R.id.tv_main_fogetpassword:
+			Toast.makeText(this, "忘记密码标签被点击", 1000).show();
+		}
+	}
+}
---
+++ src/com/gdb/guodongbu/RegisterFrame.java
@@ -0,0 +1,13 @@
+package com.gdb.guodongbu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterFrame extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+	}
+}
